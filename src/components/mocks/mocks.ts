75e2336f3cbc0c8b4dfb09b8e88b81279344d5c8
Svetlana Dyakova <sdyakova@outlook.com>
2022-10-17 15:56:33 +0300

    add task layout

---
+++ src/components/mocks/mocks.ts
@@ -0,0 +1,9 @@
+import { TaskModel } from "../types/types";
+
+let maxId = 100;
+
+export const mockData = [
+  { id: maxId++, title: "Task1" },
+  { id: maxId++, title: "Task2" },
+  { id: maxId++, title: "Task3" },
+];
